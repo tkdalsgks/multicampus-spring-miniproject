29be5395c065e1d782fe209c0ef8a91ef09b87da
tkdalsgks <hsm_01@naver.com>
2022-03-22 15:44:23 +0900

    회원가입

--- src/main/java/com/commu/team3/dto/MemberDTO.java
+++ src/main/java/com/commu/team3/dto/MemberDTO.java
@@ -1,22 +1,7 @@
 package com.commu.team3.dto;
 
 public class MemberDTO {
-	String userName, userId, userEmail, userPwd;
-	
-	public MemberDTO() {}
-	
-	public MemberDTO(String userId, String userPwd) {
-		this.userId = userId;
-		this.userPwd = userPwd;
-	}
-	
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
+	String userId, userPwd, userName, userEmail;
 
 	public String getUserId() {
 		return userId;
@@ -26,14 +11,6 @@ public class MemberDTO {
 		this.userId = userId;
 	}
 
-	public String getUserEmail() {
-		return userEmail;
-	}
-
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-
 	public String getUserPwd() {
 		return userPwd;
 	}
@@ -41,4 +18,21 @@ public class MemberDTO {
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
 	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	
 }
