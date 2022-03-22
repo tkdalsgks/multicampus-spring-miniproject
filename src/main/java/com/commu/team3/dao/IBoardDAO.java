bc452002445d64f23639725a8a4bb50fe9a15132
Seongil-Yoon <dbs3346@gmail.com>
2022-03-22 14:12:49 +0900

    1.1

---
+++ src/main/java/com/commu/team3/dao/IBoardDAO.java
@@ -0,0 +1,8 @@
+package com.commu.team3.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IBoardDAO {
+	
+}
--- src/main/java/com/commu/team3/dto/MemberDTO.java
+++ src/main/java/com/commu/team3/dto/MemberDTO.java
@@ -1,15 +1,19 @@
 package com.commu.team3.dto;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberDTO {
-	String userName, userId, userEmail, userPwd;
-	
-	public MemberDTO() {}
-	
+	private String userName, userId, userEmail, userPwd;
+
+	public MemberDTO() {
+	}
+
 	public MemberDTO(String userId, String userPwd) {
 		this.userId = userId;
 		this.userPwd = userPwd;
 	}
-	
+
 	public String getUserName() {
 		return userName;
 	}
@@ -41,4 +45,11 @@ public class MemberDTO {
 	public void setUserPwd(String userPwd) {
 		this.userPwd = userPwd;
 	}
+
+	@Override
+	public String toString() {
+		return "MemberDTO [userName=" + userName + ", userId=" + userId + ", userEmail=" + userEmail + ", userPwd="
+				+ userPwd + "]";
+	}
+
 }
